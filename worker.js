15eb2e7d6625cb4919235ce1811fc55007a6ae84
desktoping <reden@armstead.io>
2016-04-10 17:16:39 +0800

    preparation for docker

--- worker.js
+++ worker.js
@@ -1,63 +1,51 @@
 var co = require('co'),
-  dotenv = require('dotenv'),
-  kue = require("kue"),
-  fs = require('fs'),
-  req = require('superagent'),
+    each = require('co-each'),
+    dotenv = require('dotenv'),
+    kue = require("kue"),
+    fs = require('fs'),
+    req = require('superagent'),
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    q = kue.createQueue();
 
-q = kue.createQueue();
+
 dotenv.load({path: __dirname + '/.env'});
 
+mongoose.connect(process.env.MONGO_DB);
+
+// ** mongoose schema
+var Business = new Schema({
+  "License Number": String,
+  "MJBL Number": String,
+  "Business": String,
+  "Business Telephone": String,
+  "License Category": String,
+  "Status": String,
+  "Date of License": String,
+  "Out of Business Date": String,
+  "Business Name": String,
+  "Primary Jurisdiction": String,
+  "Non Primary Jurisdiction": String,
+  "Business Address": String
+});
+
+var AddBusiness = mongoose.model('business', Business);
+
 q.process('bl-jobs', function (job, done) {
-  console.log('working on job %d', job.id);
-  
-  var arrXml = [],
-      xml = '',
-      ctr = 0,
-      data = job.data;
-
-  function cleanString(string) {
-    return string.replace("\'", " ").replace("\"", " ").replace(">", " ").replace("<", " ").replace("&", "and")
-  }
-
-  console.log(data.length, 'accounts to add');
-  for (var i = 0; i < data.length; i++) {
-    var xml = '<Accounts><row no="1"><FL val="Account Name">'+ cleanString(data[i]['Business Name']) +'</FL>';
-
-    delete data[i]['Business Name']
-    for (prop in data[i]) {
-      xml += '<FL val="' + cleanString(prop) + '">' + cleanString(data[i][prop]) + '</FL>';
-    }
-    xml += '</row></Accounts>';
-
-    arrXml.push(xml);
-  }
 
   co(function *() {
-    while (ctr < arrXml.length) {
-      yield processing(arrXml[ctr]);
-      console.log(ctr, 'of ', arrXml.length, ' accounts processed.' );
-      ctr++;
-    }
-    done();
-  });
-
-  function processing(data) {
-    return new Promise(function (resolve, reject) {
-      req
-        .post('https://crm.zoho.com/crm/private/xml/Accounts/insertRecords')
-        .query({newFormat: 1})
-        .query({authtoken: process.env.ZOHO_AUTH})
-        .query({scope: 'crmapi'})
-        .query({duplicateCheck: 2})
-        .query({version: 4})
-        .query({xmlData: data})
-        .end(function (err, res) {
-          //console.log(res.text);
+    function saveToDb(data) {
+      return new Promise(function (resolve, reject) {
+        var newBusiness = new AddBusiness(data);
+        newBusiness.save(function (err) {
           if (err) return reject(err);
           return resolve();
-        });
-    });
-  }
+        })
+      });
+    };
+    yield each(job.data, saveToDb);
+    done();
+  });
 });
 
 
